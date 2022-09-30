d35cafd34323a99f760b3a01d1069e1a5ff56542
Timothy Do <timothd4@uci.edu>
2022-09-30 12:57:48 -0700

    generated live disparity map

---
+++ Vision/showCam.py
@@ -0,0 +1,17 @@
+import cv2
+
+leftCam = cv2.VideoCapture(1)
+rightCam = cv2.VideoCapture(2)
+
+while True:
+    leftCheck, leftFrame = leftCam.read()
+    rightCheck, rightFrame = rightCam.read()
+    cv2.imshow('left', leftFrame)
+    cv2.imshow('right',rightFrame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
