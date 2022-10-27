f27898ea3608667a9b27936580a13794fe6811dc
Timothy Do <timothd4@uci.edu>
2022-10-26 23:55:17 -0700

    Update cvfsm.py

--- FSM/cvfsm.py
+++ FSM/cvfsm.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 	# pins are physical
 	master = {	
 		'buttons': {
-			'power': {'pin': 5},
+			'power': {'pin': 10},
 			'capture':{'pin': 12}, 
 			'sel1': {'pin': 16},
 			'sel2': {'pin': 18}
