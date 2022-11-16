82e4278d06efd3d7707111fc183f55ca588d60a2
Timothy Do <timothd4@uci.edu>
2022-11-15 16:49:12 -0800

    Update cvfsm.py

--- FSM/cvfsm.py
+++ FSM/cvfsm.py
@@ -101,12 +101,12 @@ def capture(master):
 			'downscale':4
 		}}
 	image_L = cv.imread('../Images/left_piano.png', 1)
-    image_L = cv.cvtColor(image_L, cv.COLOR_BGR2RGB)
-    image_R = cv.imread('../Images/right_piano.png', 1)
-    image_R = cv.cvtColor(image_R, cv.COLOR_BGR2RGB)
+	image_L = cv.cvtColor(image_L, cv.COLOR_BGR2RGB)
+	image_R = cv.imread('../Images/right_piano.png', 1)
+	image_R = cv.cvtColor(image_R, cv.COLOR_BGR2RGB)
 
-    image_L_gray = cv.cvtColor(image_L, cv.COLOR_BGR2GRAY) 
-    image_R_gray = cv.cvtColor(image_R, cv.COLOR_BGR2GRAY) 
+	image_L_gray = cv.cvtColor(image_L, cv.COLOR_BGR2GRAY) 
+	image_R_gray = cv.cvtColor(image_R, cv.COLOR_BGR2GRAY) 
 	cs.processCapture(image_L_gray,image_R_grayconfig[master['settings']['mode']]['algor'],config[master['settings']['mode']]['downscale'])
 	rightPreview(master)
 
