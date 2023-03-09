2ae92d54eb73fa0e5b92aed561d15b5796175636
Timothy Do <timothd4@uci.edu>
2023-03-08 21:26:28 -0800

    sure...

--- Images/Pillow/getSGBMCurve.m
+++ Images/Pillow/getSGBMCurve.m
@@ -6,26 +6,29 @@ bf = zeros(size(depths));
 
 for i = 1:size(depths,2)
     depth = depths(i);
-    SGBM = readmatrix("SGBM_" + string(depth) + ".csv");
-    disps(i) = SGBM(255,239);
+    SGBM = readmatrix("Scan_256/SGBM_" + string(depth) + ".csv");
+    disps(i) = SGBM(245,90);
     bf(i) = depths(i)*disps(i); 
 end
 
 clear SGBM; clear i; clear depth;
 
-figure;
+f = figure;
 plot(disps,depths,'LineWidth',2);
-grid on;
-title('SGBM Disparity vs Depths');
-xlabel('OpenCV SGBM Disparity (Pixels)');
-ylabel('Measured Depth (in.)');
+grid on; 
+set(gca,"FontSize",18);
+t = title('Disparity to Depth');
+t.FontSize = 24;
+xlabel("Disparity x_L' - x_R' [Pixels]");
+ylabel('Depth [inches]');
+saveas(f,'dispvsdepth.png');
 
-figure;
-plot(depths,bf,'LineWidth',2);
-grid on;
-title('Measured Depths vs bf');
-xlabel('Measured Depths (in.)');
-ylabel('bf');
+% figure;
+% plot(depths,bf,'LineWidth',2);
+% grid on;
+% title('Measured Depths vs bf');
+% xlabel('Measured Depths (in.)');
+% ylabel('bf');
 
 
 data = transpose(cat(1,disps,depths));
