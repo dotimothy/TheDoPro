9234a15449a7940a0f24bed5dac9f2da358a928f
Timothy Do <timothd4@uci.edu>
2023-03-08 22:09:15 -0800

    Modified battery metric, made it look better

--- FSM/plotBatteryMetric.m
+++ FSM/plotBatteryMetric.m
@@ -6,12 +6,13 @@ f = figure;
 plot(data.TimeElapsed_Minutes_,data.BatteryVoltage_Volts_,'LineWidth',2);
 title('Battery Life Expectancy');
 set(gca,"FontSize",12);
-xlabel('Time (Minutes)');
-ylabel('Battery Voltage (V)');
+xlabel('Time [Minutes]');
+ylabel('Battery Voltage [Volts]');
 hold on;
 axis([0 90 3 4.2]);
 yline(3.2,'LineWidth',2,'Color',[1 0 0]);
-legend('Battery Voltage','Threshold');
+xline(77,'--','LineWidth',2,'Color', [0 0.5 0]);
+legend('Battery Voltage','Cut-off Voltage (V = 3.2 V)','Cut-off Time (T = 77 mins)');
 grid on;
 saveas(f,'batteryMetric.jpg');
 
