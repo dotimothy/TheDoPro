eaac9083c69f98caaaeb01b05d1201f88b6f8f30
Timothy Do <timothd4@uci.edu>
2023-03-10 17:07:13 -0800

    Update cvfsm.py

--- FSM/cvfsm.py
+++ FSM/cvfsm.py
@@ -130,15 +130,15 @@ def setupPreview(root,master,lbl):
 			widget.destroy()
 	lbl.grid(row=1,column=1)
 	if(master['settings']['state'] != 'Capture'):
-		tk.Button(root,text="Change Camera",font=("Courier",12),command=lambda:changeCamera(master)).grid(row=2,column=1,padx=10,pady=10)
+		tk.Button(root,text="Change Camera",font=("Courier",14),command=lambda:changeCamera(master)).grid(row=2,column=1,padx=10,pady=10)
 	else:
-		tk.Button(root,text="Revert",font=("Courier",12),command=lambda:changeCamera(master)).grid(row=2,column=1,padx=10,pady=10)
-	tk.Button(root,text="Save Image",font=("Courier",12),command=lambda:turnOnSave(master)).grid(row=1,column=2,padx=10,pady=10)
-	tk.Button(root,text="Gallery",font=("Courier",12),command=lambda:openGallery('results')).grid(row=1,column=3,padx=10,pady=10)
-	tk.Button(root,text="Capture",font=("Courier",12),command=lambda:updateState(master,'Capture')).grid(row=2,column=2,padx=10,pady=10)
-	tk.Button(root,text="Settings",font=("Courier",12),command=lambda:configSettings(master)).grid(row=2,column=3,padx=10,pady=10)
+		tk.Button(root,text="Revert",font=("Courier",14),command=lambda:changeCamera(master)).grid(row=2,column=1,padx=10,pady=10)
+	tk.Button(root,text="Save Image",font=("Courier",14),command=lambda:turnOnSave(master)).grid(row=1,column=2,padx=10,pady=10)
+	tk.Button(root,text="Gallery",font=("Courier",14),command=lambda:openGallery('results')).grid(row=1,column=3,padx=10,pady=10)
+	tk.Button(root,text="Capture",font=("Courier",14),command=lambda:updateState(master,'Capture')).grid(row=2,column=2,padx=10,pady=10)
+	tk.Button(root,text="Settings",font=("Courier",14),command=lambda:configSettings(master)).grid(row=2,column=3,padx=10,pady=10)
 	if(programMode == 1):
-		tk.Button(root,text="Correct",font=("Courier",12),command=lambda:cs.correctPosition()).grid(row=3,column=3,padx=10,pady=10)
+		tk.Button(root,text="Correct",font=("Courier",14),command=lambda:cs.correctPosition()).grid(row=3,column=3,padx=10,pady=10)
 
 def updateState(master,state):
 	master['settings']['state'] = state
@@ -171,7 +171,7 @@ def saveImage(im,outputDir):
 	cv.imwrite(outputPath,cv.cvtColor(im,cv.COLOR_RGB2BGR))
 	root = tk.Tk()
 	root.title('Saving')
-	tk.Label(root,text=f'Saved Output to {outputPath}',font=("Courier",12)).pack()
+	tk.Label(root,text=f'Saved Output to {outputPath}',font=("Courier",14)).pack()
 	root.after(2000,root.destroy)
 
 def configSettings(master):
@@ -184,7 +184,7 @@ def configSettings(master):
 	title = tk.Label(root,text='Settings',font=("Courier",30))
 	title.grid(row=0,column=1)
 	
-	modeLabel = tk.Label(root,text="Disparity Mode: ",font=("Courier",12))
+	modeLabel = tk.Label(root,text="Disparity Mode: ",font=("Courier",14))
 	modeLabel.grid(row=1,column=1)
 	modes = ['OpenCV_BM','OpenCV_SGBM','Cost Block','Multiblock']
 	mode = tk.StringVar(root)
@@ -195,7 +195,7 @@ def configSettings(master):
 	modeSelections.configure(font=tkFont.Font(family='Arial',size=18,weight=tkFont.BOLD))
 	modeSelection.grid(row=1,column=2)
 
-	rectLabel = tk.Label(root,text="Rectification: ",font=("Courier",12))
+	rectLabel = tk.Label(root,text="Rectification: ",font=("Courier",14))
 	rectLabel.grid(row=2,column=1)
 	rectifications = ['On','Off']
 	rectification = tk.StringVar(root)
@@ -206,7 +206,7 @@ def configSettings(master):
 	rectSelections.configure(font=tkFont.Font(family='Arial',size=18,weight=tkFont.BOLD))
 	rectSelection.grid(row=2,column=2)
 
-	cmapLabel = tk.Label(root,text="Color Map: ",font=("Courier",12))
+	cmapLabel = tk.Label(root,text="Color Map: ",font=("Courier",14))
 	cmapLabel.grid(row=3,column=1)
 	cmapModes = ['Jet','Gray','Bone','Rainbow','HSV','Viridis']
 	cmap = tk.StringVar(root)
@@ -220,9 +220,9 @@ def configSettings(master):
 	disparity = tk.StringVar(root)
 	disparity.set(master['settings']['disparity'])
 	if(master['settings']['mode'] == 'OpenCV_SGBM' or master['settings']['mode'] == 'OpenCV_BM'):
-		disLabel = tk.Label(root,text="Disparity Range: ",font=("Courier",12))
+		disLabel = tk.Label(root,text="Disparity Range: ",font=("Courier",14))
 		disLabel.grid(row=4,column=1)
-		disModes = [16,32,64,112,256]
+		disModes = [16,32,64,114,256]
 		disSelection = tk.OptionMenu(root,disparity,*disModes)
 		disSelection.config(font=tkFont.Font(family='Arial',size=24,weight=tkFont.BOLD))
 		disSelections = root.nametowidget(disSelection.menuname)
@@ -232,7 +232,7 @@ def configSettings(master):
 	relative = tk.StringVar(root)
 	relative.set(master['settings']['relative'])
 	if(master['settings']['mode'] == 'OpenCV_SGBM' or master['settings']['mode'] == 'OpenCV_BM'):
-		reLabel = tk.Label(root,text="Relative: ",font=("Courier",12))
+		reLabel = tk.Label(root,text="Relative: ",font=("Courier",14))
 		reLabel.grid(row=5,column=1)
 		reModes = ['On','Off']
 		reSelection = tk.OptionMenu(root,relative,*reModes)
@@ -244,7 +244,7 @@ def configSettings(master):
 	flash = tk.StringVar(root)
 	flash.set(master['settings']['flash'])
 	if(programMode == 1 and sys.platform == 'linux'):
-		flashLabel = tk.Label(root,text="Flash: ",font=("Courier",12))
+		flashLabel = tk.Label(root,text="Flash: ",font=("Courier",14))
 		flashLabel.grid(row=6,column=1)
 		flashModes = ['On','Off']
 		flashSelection = tk.OptionMenu(root,flash,*flashModes)
@@ -256,16 +256,16 @@ def configSettings(master):
 	exposure = tk.StringVar(root)
 	exposure.set(master['settings']['exposure'])
 	if(programMode == 1):
-		exposLabel = tk.Label(root,text="Exposure: ",font=("Courier",12))
+		exposLabel = tk.Label(root,text="Exposure: ",font=("Courier",14))
 		exposLabel.grid(row=7,column=1)
-		exposModes = [-1.0,-2.0,-3.0,-4.0,-5.0,-6.0,-7.0,-8.0,-10.0,-11.0,-12.0,-13.0,-14.0]
+		exposModes = [-1.0,-2.0,-3.0,-4.0,-5.0,-6.0,-7.0,-8.0,-10.0,-11.0,-14.0,-13.0,-14.0]
 		exposSelection = tk.OptionMenu(root,exposure,*exposModes)
 		exposSelection.config(font=tkFont.Font(family='Arial',size=24,weight=tkFont.BOLD))
 		exposSelections = root.nametowidget(exposSelection.menuname)
 		exposSelections.configure(font=tkFont.Font(family='Arial',size=18,weight=tkFont.BOLD))
 		exposSelection.grid(row=7,column=2)
 
-	confirm = tk.Button(root,text="Update Settings",font=("Courier",12),command=lambda:updateSettings(master,mode.get(),rectification.get(),cmap.get(),disparity.get(),relative.get(),flash.get(),exposure.get(),root))
+	confirm = tk.Button(root,text="Update Settings",font=("Courier",14),command=lambda:updateSettings(master,mode.get(),rectification.get(),cmap.get(),disparity.get(),relative.get(),flash.get(),exposure.get(),root))
 	confirm.grid(row=8,column=2)
 	
 	root.mainloop()
