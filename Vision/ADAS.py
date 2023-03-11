dbd3c0de88df55a0d45f9c31f6ba1a1f592f9a63
Timothy Do <timothd4@uci.edu>
2023-03-10 17:52:16 -0800

    Update ADAS.py

--- Vision/ADAS.py
+++ Vision/ADAS.py
@@ -176,7 +176,7 @@ def imagePreview(root,master,lbl):
 		root.title(f'TheDoPro ADAS System Detected: {detect}')
 		if(sys.platform == 'linux'):
 				for led in master['leds']: #Turns on Light if Detected.
-					GPIO.output(master['leds'][led]['pin'],1 if detect else 0)
+					GPIO.output(master['leds'][led]['pin'],detect)
 
 	if(master['settings']['save'] == 'On'):
 		if(not os.path.exists('./results')):
