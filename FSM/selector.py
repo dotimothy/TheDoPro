afa48253caf575900c5eff19102a7054047be86f
Timothy Do <timothd4@uci.edu>
2023-03-10 16:52:06 -0800

    Update selector.py

--- FSM/selector.py
+++ FSM/selector.py
@@ -27,6 +27,7 @@ def shutdown():
 
 def setupPreview(root,lbl,exit):
 	root.title('TheDoPro Program Selector')
+	root.geometry('240x320')
 	if(sys.platform == 'win32'):
 		root.iconbitmap('../Images/favicon.ico')
 	lbl.grid(row=1,column=1,padx=10,pady=10)
