fd7d967e84bb9fe78a95084f1c351027611f3f4f
Timothy Do <timothd4@uci.edu>
2023-03-18 20:26:00 -0700

    Fixed some errors

---
+++ docs/thedopro.js
@@ -0,0 +1,6 @@
+function viewPaper() {
+	document.getElementById('doc').innerHTML = `<iframe width="100%" height="95%" frameborder="0" src="https://docs.google.com/gview?url=https://github.com/dotimothy/TheDoPro/raw/main/Documentation/The_Do_Pro_%20A_Simplistic_Stereovision_Camera.pdf&embedded=true"></iframe>`
+}
+function viewPoster() {
+	document.getElementById('doc').innerHTML = `<iframe width="100%" height="95%" frameborder="0" src="https://docs.google.com/gview?url=https://github.com/dotimothy/TheDoPro/raw/main/Documentation/PictureThis_Poster_Final.pdf&embedded=true"></iframe>`
+}
